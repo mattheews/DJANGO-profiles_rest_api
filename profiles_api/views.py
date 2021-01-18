5eb9382dcda0526c1699c9fe3099948740f7cf04
mslinuxdev <58988749+mslinuxdev@users.noreply.github.com>
2021-01-18 06:39:21 +0100

    Feature/apiviews (#9)
    * Get ApiView
    
    * Added POST to HEllo-View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, path, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives You the most control over Your application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
