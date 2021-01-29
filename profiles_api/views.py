6b340e32ef4bce151393f7a1f854c5634ae4419a
mslinuxdev <58988749+mslinuxdev@users.noreply.github.com>
2021-01-29 05:49:46 +0100

    Feature/user profile (#13)
    * Created user profile
    
    * Updated rest calls
    
    * Added permissions to profiles
    
    * Added search filter
    
    * Added login page
    
    * Added login page
    
    * Added login page

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from profiles_api import serializers
+from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-app_version = "1.3"
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+app_version = "2.3"
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -78,6 +85,21 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
 
     
     
