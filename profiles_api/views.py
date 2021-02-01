a9c7caa22ce270c3870c0945e8abff8439764459
Mateusz Sobocinski <mateusz.sobocinski7912@gmail.com>
2021-02-01 08:28:20 +0100

    Added permissions to profiles_api - feed and user_profile)

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,10 +4,11 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import models, permissions, serializers
 
-app_version = "3.1"
+app_version = "3.4"
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -87,7 +88,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
+    permission_classes = (permissions.UpdateOwnProfile, IsAuthenticated)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
@@ -100,6 +101,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
         """Sets the user profile to the loged in user"""
